598036a0f5617923492e4e82a06d3a4787c95820
billybolton <billybolton16@gmail.com>
2022-03-10 11:33:22 -0500

    stripped

--- api/src/main/java/ca/me/proj/repository/base/IQdslRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.me.proj.repository.base;
-
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface IQdslRepository<E> extends IBaseRepository<E>, QuerydslPredicateExecutor<E> {
-
-}

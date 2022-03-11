bb6d1a43d201120f6ab8b18d92f38c07abe8f914
billybolton <billybolton16@gmail.com>
2022-03-11 18:10:59 -0500

    base abstracted DTO and entity with id name change

--- shared/src/main/java/ca/me/proj/dtos/base/BaseDTO.java
+++ shared/src/main/java/ca/me/proj/dtos/base/BaseDTO.java
@@ -6,10 +6,4 @@ import lombok.Data;
 @Data
 public class BaseDTO implements IBaseDTO, Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6672771950395342891L;
-	private Long entityId;
-
 }
